8341ff5790d256c18512c36eefe45c293e54f891
Iris0328 <yutinghuang0328@gmail.com>
2025-03-30 18:42:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -7,8 +7,15 @@
  */
 
 
+// 問卷網址列表
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+       'https://www.surveycake.com/s/W021x',
+       'https://www.surveycake.com/s/86ZW3', 
+       'https://www.surveycake.com/s/reLNv',
+       'https://www.surveycake.com/s/0zA49',
+       'https://www.surveycake.com/s/wrqKl', 
 ];
+
+// 隨機選擇一個 URL 並跳轉
+const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+window.location.href = randomUrl;
